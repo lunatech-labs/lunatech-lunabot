23494b54dece7bf30d2af11189b5bc3502ce6eed
dcharoulis <dimitrios.charoulis@lunatech.com>
2015-10-15 16:42:58 +0200

    Fixes #21 Add multiple room integration. Provide improvements for Fixes #14 and Fixes #10 Program can recieve requests from multiple rooms and can respond to each by checking "application.conf" configuration file. Made cleaner code for #14 ( add new functions prepareResponse and executeExpression ). Migrate above changes to #10.

--- src/main/scala/com/lunatech/lunabot/LunabotServlet.scala
+++ src/main/scala/com/lunatech/lunabot/LunabotServlet.scala
@@ -5,11 +5,11 @@ import org.scalatra._
 import org.json4s.{DefaultFormats, Formats}
 import org.slf4j.LoggerFactory
 import org.scalatra.json._
-import scala.sys.process._
 import dispatch._, Defaults._
 import scala.util.{Failure, Success}
-
-
+import org.json4s._
+import org.json4s.JsonDSL._
+import com.typesafe.config.ConfigFactory
 /**
  * Application to handle POST requests received from HipChat.
  *
@@ -20,9 +20,10 @@ import scala.util.{Failure, Success}
  */
 class LunabotServlet extends ScalatraServlet with JacksonJsonSupport {
 
-  val logger = LoggerFactory.getLogger(getClass)
+  protected implicit val jsonFormats: Formats = DefaultFormats
 
-  protected implicit val jsonFormats: Formats = DefaultFormats.withBigDecimal
+  val logger = LoggerFactory.getLogger(getClass)
+  val configFactory = ConfigFactory.load()
 
   before() {
     contentType = formats("json")
@@ -36,38 +37,41 @@ class LunabotServlet extends ScalatraServlet with JacksonJsonSupport {
     logger.debug(request.body)
     val jsonValue = parse(request.body.replace("mention_name", "mentionName"))
     val hipchatMsg = jsonValue.extract[HipChatMessage]
-    val authToken = System.getenv("AUTH_TOKEN")
-    val urlStr = "https://api.hipchat.com/v2/room/roomId/notification?auth_token="
+    val cmd: String = hipchatMsg.item.message.message.replace("/scala ", "")
 
-    val executionFuture: Future[String] = Future {
-      val cmd1: String = hipchatMsg.item.message.message.replace("/scala ", "")
-      REPLproc.run(cmd1)
-    }
 
-    executionFuture.onComplete {
-      case Success(result) => {
-        //Send response to HipChat Room
-        val myRequest = sendAsyncResponse(urlStr, hipchatMsg, authToken) <<
-          """{"color": "green", "message": """ + "\"" + "@" + hipchatMsg.item.message.from.mentionName + " " +
-            result.replace("\n", "\\n") + "\"" + """, "notify": false, "message_format": "text"}"""
+
+    //Send response to HipChat Room.
+    executeExpression(cmd).onComplete {
+      case Success(result) =>
+        val jsonResponse: String = compact(render(("color" -> "green") ~
+          ("message" -> result) ~
+          ("notify" -> false) ~
+          ("message_format" -> "text")))
+        val myRequest: Req = prepareResponse(hipchatMsg) << jsonResponse
         dispatch.Http(myRequest)
-      }
-      case Failure(ex: Exception) => {
-        val myRequest = sendAsyncResponse(urlStr, hipchatMsg, authToken) <<
-          s"""{"color": "red", "message": "Sorry your scala code has failed with ${ex.getMessage}
-             |", "notify": false, "message_format": "text"}""".stripMargin
+
+      case Failure(ex: Exception) =>
+        val errorResp = compact(render(("color" -> "red") ~
+          ("message" -> s"Sorry your scala code has failed with ${ex.getMessage}") ~
+          ("notify" -> false) ~
+          ("message_format" -> "text")))
+        val myRequest = prepareResponse(hipchatMsg) << errorResp
         dispatch.Http(myRequest)
-      }
-
     }
-
   }
 
-  def sendAsyncResponse(urlStr: String, hipchatMsg: HipChatMessage, authToken: String): Req = {
+
+  def prepareResponse(hipchatMsg: HipChatMessage): Req = {
     val roomId = hipchatMsg.item.room.id
-    val updatedUrl = urlStr.replace("roomId", roomId.toString).concat(authToken)
-    val request = dispatch.url(updatedUrl).setContentType("application/json", "UTF-8").POST
-    request
+    val authToken = configFactory.getString(s"AUTH_TOKENS.$roomId.token")
+    val urlStr = s"https://api.hipchat.com/v2/room/$roomId/notification?auth_token=$authToken"
+    val myRequest = dispatch.url(urlStr).setContentType("application/json", "UTF-8").POST
+    myRequest
   }
-}
 
+  def executeExpression(scalaExpr: String): Future[String] = Future {
+    REPLproc.run(scalaExpr)
+  }
+
+}
